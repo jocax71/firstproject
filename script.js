14f3916e4e652a546652c5340125dc2b03844008
jocax71 <joaocarneir.ox71@gmail.com>
2025-11-26 20:48:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var nome = prompt("Digite o nome do visitante: ");
+alert ("Seja bem vindo " + nome);
+alert ("Pagina feita com o objetivo de documentar minha jornada no mundo do desenvolvimento e da tecnologia.");
